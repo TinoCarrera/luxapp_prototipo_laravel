66b9fc856f21d95896166e5cd1b7e6b5129487b9
Florentino Carrera <70342537+TinoCarrera@users.noreply.github.com>
2020-09-14 17:46:46 -0400

    Autenticación JWT:
    -Creación de rutas.
    -Creación del controlador User.

--- routes/api.php
+++ routes/api.php
@@ -3,16 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('register','Api\UserController@register');
+Route::post('login','Api\UserController@login');
+Route::post('update','Api\UserController@update');
+Route::get('logout','Api\UserController@logout');
+Route::post('user','Api\UserController@getCurrentUser');
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
